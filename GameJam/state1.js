af9491c7893ed28b0477e025f776f200d69e4d68
haydenc36 <haydenc36@yahoo.com>
2017-09-23 23:41:52 -0500

    Updated map with sprites

--- GameJam/state1.js
+++ GameJam/state1.js
@@ -1,17 +1,25 @@
 // ENGLAND VILLAGE
 var demo = {};
+var monk, speed = 5;
+
 demo.state1 = function(){};
 demo.state1.prototype = {
+    
     preload: function(){
-        
+        game.load.spritesheet('monk', '../assets/spritesheets/monk.png', 32, 32);
         game.load.tilemap('england_village', '../assets/tilemaps/england_village.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('magecity', '../assets/tilemaps/magecity.png');
         game.load.image('wood_tileset', '../assets/tilemaps/wood_tileset.png');
-        
     },
+    
     create:function(){
-        //game.stage.backgroundColor = '#DDDDDD';
-
+        // Initialize Physics
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        //Adjust camera settings
+        game.world.setBounds(0,0, 3200, 3200);
+        //game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
         var map = game.add.tilemap('england_village');
         map.addTilesetImage('magecity');
         map.addTilesetImage('wood_tileset');
@@ -25,7 +33,54 @@ demo.state1.prototype = {
         var windows = map.createLayer('windows');
         var stairs = map.createLayer('stairs');
         var buildings = map.createLayer('buildings');
-
+        
+        // Initialize the monk character
+        monk = game.add.sprite(0, 0, 'monk');
+        monk.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(monk);
+        monk.body.collideWorldBounds = true;
+        monk.animations.add('walk', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16, 17, 18, 19, 20]);
+        
+        // Adjust the camera
+        game.camera.follow(monk);
+        game.camera.deadzone = new Phaser.Rectange(0, 800, 800, 800);
+        
+        // Controls
+        cursors = game.input.keyboard.createCursorKeys();
     },
-    update: function(){}
+    
+    update: function(){
+        
+        // Set movement controls
+        // MOVING
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP) || game.input.keyboard.isDown(Phaser.Keyboard.DOWN) || game.input.keyboard.isDown(Phaser.Keyboard.LEFT) || game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+
+            if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+                monk.y -= speed;
+                monk.animations.play('walk', 5, true);
+            }
+            else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+                monk.y += speed;
+                monk.animations.play('walk', 5, true);
+            }
+
+            // LEFT AND RIGHT
+            if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+                monk.scale.setTo(-1, 1);
+                monk.x += speed;
+                monk.animations.play('walk', 5, true);
+            }
+            else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+                monk.scale.setTo(1, 1);
+                monk.x -= speed;
+                monk.animations.play('walk', 5, true);
+            }
+        }
+
+        // NOT MOVING
+        else{
+            monk.animations.stop('walk');
+            monk.frame = 0;
+        }
+    }
 };
