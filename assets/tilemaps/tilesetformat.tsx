51dd376ab2a4c5ac03fcb1bf02ccb4061936d6c0
haydenc36 <haydenc36@yahoo.com>
2017-09-23 20:50:53 -0500

    Updated JSON Tilemap

---
+++ assets/tilemaps/tilesetformat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tilesetformattedupdate1" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="tilesetformattedupdate1.png" width="112" height="192"/>
+</tileset>
