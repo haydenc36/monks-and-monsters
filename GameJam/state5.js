ea7af8128759b3d06c83b50174f4e770b10402a2
loylm <loy.lm@utexas.edu>
2017-09-24 00:19:42 -0500

    state5 update
    Probably need to edit dimensions

--- GameJam/state5.js
+++ GameJam/state5.js
@@ -1,6 +1,20 @@
+//Monster Lair
 demo.state5 = function(){};
 demo.state5.prototype = {
-    preload: function(){},
-    create:function(){},
+    preload: function(){
+        game.load.tilemap('MonsterLair', '../assets/tilemaps/MonsterLair.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('ground', '../assets/tilemaps/ground_tiles.png');
+        game.load.image('obj', '../assets/tilemaps/object-layer.png');
+    },
+    create:function(){
+        var map = game.add.tilemap('MonsterLair');
+        map.addTilesetImage('ground');
+        map.addTilesetImage('obj');
+        
+        var sand = map.createLayer('Sand');
+        var sanddeets = map.createLayer('SandDetails');
+        var rocks = map.createLayer('Rock');
+        var bar = map.createLayer('Barrel');
+    },
     update: function(){}
 };
