61082aa9bb93be86786fbb7542da67c8fb69aab2
loylm <loy.lm@utexas.edu>
2017-09-29 18:49:12 -0500

    TestGithub

--- mainBranch/state9.js
+++ mainBranch/state9.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-hello
+mochi
 // Satan's Temple
-=======
-// Satan's Temple bsafbasbdfhksabfhksbdfk
->>>>>>> 7c067d3a9dda00f6d591e048c452585a5eaba8d9
 demo.state9 = function(){};
 demo.state9.prototype = {
     preload: function(){},
