f0c7e522c6b98b42e4007c23347d1226f8f686b4
haydenc36 <haydenc36@yahoo.com>
2017-10-01 23:19:47 -0500

    Debugging States 1 - 7

--- assets/tilemaps/files/village_tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="village_tileset2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="village_tileset2.png" width="512" height="512"/>
-</tileset>
