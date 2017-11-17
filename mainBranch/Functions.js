c940e51893fd5f22d4c859b777c1c448623f2fb8
Zhenyu Zeng <tonyzeng76@gmail.com>
2017-11-17 11:23:58 -0600

    Inventory centered

--- mainBranch/Functions.js
+++ mainBranch/Functions.js
@@ -883,8 +883,8 @@ createInventory = function (game_state){
 	   //GUI - the background image for the inventory
 	   game_state.inventory_base = game_state.add.sprite(game.world.centerX, game.world.centerY,  'inventory_base');
 	   game_state.inventory_base.fixedToCamera = true;
-	   game_state.inventory_base.cameraOffset.x = 500;
-	   game_state.inventory_base.cameraOffset.y = 500;
+	   game_state.inventory_base.cameraOffset.x = 365;
+	   game_state.inventory_base.cameraOffset.y = 190;
         game_state.inventory_base.visible = false;
         game_state.inventory_base.scale.set(1.5);
         
